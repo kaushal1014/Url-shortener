652351a5bc68b6f4fc3f1fcab2a6f4b70099ab72
Kaushal Patil <patilkshl@gmail.com>
2020-10-23 23:36:59 +0530

    User registeration done, has a few gaps

--- urlshortener/authenticate/views.py
+++ urlshortener/authenticate/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login , logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
+from django.contrib.auth.models import User
 # Create your views here.
 def login_view(request):
     if request.method=='POST':
@@ -16,6 +17,16 @@ def login_view(request):
     return render(request, "login.html")
 
 def signup(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        # password=request.POST['password'] not needed at the moment
+        confirm_password=request.POST['password1']
+        # email= request.POST['email']  not needed at the moment
+        confirm_email= request.POST['email1']
+        user=User.objects.create_user(username=username, password=confirm_password, email=confirm_email)
+        user.save()
+        return HttpResponseRedirect(reverse('urlshortner'))
+
     return render(request, "signup.html")
 
 def urlshortner(request):
