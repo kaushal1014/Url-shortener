fbc057858298617f9e078e87505dc433cfe13954
Kaushal Patil <patilkshl@gmail.com>
2020-10-25 18:22:50 +0530

    added user models

--- urlshortener/urlhandel/models.py
+++ urlshortener/urlhandel/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-
+from authenticate.models import User
 # Create your models here.
 class short_urls(models.Model):
     short_url=models.CharField(max_length=20)
     long_url=models.URLField('Url', unique=True)
-
+    
     def __str__(self):
         return f"{self.short_url} is for {self.long_url}"
