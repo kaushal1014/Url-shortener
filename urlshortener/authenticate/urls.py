5b65f6299fa56320fc8fc011f351ae4cf6bbd17b
Kaushal Patil <patilkshl@gmail.com>
2020-11-14 11:28:50 +0530

    changed redirection

--- urlshortener/authenticate/urls.py
+++ urlshortener/authenticate/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 app_name='authenticate'
 urlpatterns=[
-    path('', views.login_view, name="login"),
+    path('login/', views.login_view, name="login"),
     path('signup/', views.signup, name="signup"),
     path("urlshortner/", views.urlshortner, name='urlshortner')
 ]
